3b8dfefb3545de825541ee9ecb6b7d3f23c4dac7
Carl Dawson <carl@invisible-gorilla.com>
2017-10-03 03:09:56 -0400

    Updated course interface to match database

--- shrpr/src/app/course.interface.ts
+++ shrpr/src/app/course.interface.ts
@@ -1,11 +1,12 @@
 export interface Course {
 	id: number;	
 	name: string;
-	instructor_id: string;
+	instructor_id: number;
 	address: string;
 	city: string;
 	state: string;
 	zip: string;
 	amount: string;
+	availability: string;
 
 }
