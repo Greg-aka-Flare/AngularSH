49b5d04c4bcf5264b1d12067fae914b855759cff
Carl Dawson <carl@invisible-gorilla.com>
2017-10-12 22:36:39 -0400

    Added swipe functionality for slideshow, included hammer.js library

--- shrpr/src/app/courses/courses.component.ts
+++ shrpr/src/app/courses/courses.component.ts
@@ -41,39 +41,6 @@ export class CoursesComponent implements OnInit {
     );
     this.courses.splice(position, 1);
   }
-
-  // constant for swipe action: left or right
-  isVisible:boolean = false;
-  isHide:boolean = true;
-
-  SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
-
-  // action triggered when user swipes
-    swipe(currentIndex: number, action = this.SWIPE_ACTION.RIGHT) {
-        // out of range
-        if (currentIndex > this.courses.length || currentIndex < 0) return;
-        this.isVisible = !this.isVisible;
-        this.isHide = !this.isHide;
-
-        let nextIndex = 0;
-
-        // swipe right, next avatar
-        if (action === this.SWIPE_ACTION.RIGHT) {
-            const isLast = currentIndex === this.courses.length - 1;
-            nextIndex = isLast ? 0 : currentIndex + 1;
-
-        }
-
-        // swipe left, previous avatar
-        if (action === this.SWIPE_ACTION.LEFT) {
-            const isFirst = currentIndex === 0;
-            nextIndex = isFirst ? this.courses.length - 1 : currentIndex - 1;
-        }
-
-        // toggle avatar visibility
-
-        //this.courses.forEach((x, i) => x. = (i === nextIndex));
-    }
 }
 
 
