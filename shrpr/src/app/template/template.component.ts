4aa92bce411e46abcedffa1156c5b5d06d439537
Carl Dawson <carl@invisible-gorilla.com>
2017-10-23 21:32:26 -0400

    Added reverse geolocation to header

--- shrpr/src/app/template/template.component.ts
+++ shrpr/src/app/template/template.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 import { NgZone } from '@angular/core';
 
+declare var google;
+
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
@@ -27,6 +29,10 @@ export class TemplateComponent implements OnInit {
 export class TemplateHeader implements OnInit {
 
   isheaderShrunk: boolean = false;
+  isBtnActive: boolean = false;
+  location: string = '';
+  lat;
+  lng;
   
     constructor(zone: NgZone) {
       window.onscroll = () => {
@@ -41,13 +47,86 @@ export class TemplateHeader implements OnInit {
     }
 
   ngOnInit() {
+    if (navigator.geolocation) { //check if we can get lat/lng
+
+      //create location
+      navigator.geolocation.getCurrentPosition(position => {
+
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+
+        let geocoder = new google.maps.Geocoder();
+        let latlng = new google.maps.LatLng(this.lat, this.lng);
+
+        let request = {
+          latLng: latlng
+        };
+
+        //reverse geocode
+        geocoder.geocode(request, (results, status) => {
+
+          if (status == google.maps.GeocoderStatus.OK) {
+
+            if (results[0] != null) {
+
+              //get location
+              this.location = this.findLocation(results[0].address_components);
+            }
+          }
+        });
+      });
+    }
   }
-   //menuState:string = 'out';
-  isBtnActive:boolean = false;
+
   toggleMenu() {
-    // 1-line if statement that toggles the value:
-    //this.menuState = this.menuState === 'out' ? 'in' : 'out';
     this.isBtnActive = !this.isBtnActive;
+  }
+
+  findLocation(components){      
+
+    var city = false,
+        state = false,
+        zip = false,
+        component,
+        i, l, x, y;
+
+      for(i = 0, l = components.length; i < l; ++i){
+
+        //store component
+        component = components[i];
+
+        //check each type
+        for(x = 0, y = component.types.length; x < y; ++ x){
+
+          //depending on type, assign to var
+          switch(component.types[x]){
+
+            case 'neighborhood':
+            city = component.long_name;
+            break;
+
+            case 'administrative_area_level_1':
+            state = component.short_name;
+            break;
+
+            case 'postal_code':
+            zip = component.short_name;
+            break;
+          }
+        }
+      }
+
+      if(city && state && zip){
+
+        return city + ', ' + state + ' ' + zip;
+      }
+      else if (city && state) { 
+
+        return city + ', ' + state;
+      } else {
+
+        return '';
+      }
   }
 }
 
