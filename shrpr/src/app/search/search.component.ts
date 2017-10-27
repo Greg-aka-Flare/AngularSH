932ddedc8ef26ad6a443e167812d5d32eade5dea
oshankpandey <32125530+oshankpandey@users.noreply.github.com>
2017-10-27 18:51:40 +0530

    search ts file update for google map location

--- shrpr/src/app/search/search.component.ts
+++ shrpr/src/app/search/search.component.ts
@@ -1,15 +1,90 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var google;
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+  location: string = '';
+  groups = ['For Fun', 'For Work', 'For Kids'];
   constructor() { }
 
   ngOnInit() {
+    if (navigator.geolocation) { //check if we can get lat/lng
+      
+            //create location
+            navigator.geolocation.getCurrentPosition(position => {
+      
+              let lat = position.coords.latitude;
+              let lng = position.coords.longitude;
+      
+              let geocoder = new google.maps.Geocoder();
+              let latlng = new google.maps.LatLng(lat, lng);
+      
+              let request = {
+                latLng: latlng
+              };
+      
+              //reverse geocode
+              geocoder.geocode(request, (results, status) => {
+      
+                if (status == google.maps.GeocoderStatus.OK) {
+      
+                  if (results[0] != null) {
+      
+                    //get location
+                    this.location = this.findLocation(results[0].address_components);
+                  }
+                }
+              });
+            });
+          }
   }
-
+  findLocation(components){      
+    
+        var city = false,
+            state = false,
+            zip = false,
+            component,
+            i, l, x, y;
+    
+          for(i = 0, l = components.length; i < l; ++i){
+    
+            //store component
+            component = components[i];
+    
+            //check each type
+            for(x = 0, y = component.types.length; x < y; ++ x){
+    
+              //depending on type, assign to var
+              switch(component.types[x]){
+    
+                case 'neighborhood':
+                city = component.long_name;
+                break;
+    
+                case 'administrative_area_level_1':
+                state = component.short_name;
+                break;
+    
+                case 'postal_code':
+                zip = component.short_name;
+                break;
+              }
+            }
+          }
+    
+          if(city && state && zip){
+    
+            return city + ', ' + state + ' ' + zip;
+          }
+          else if (city && state) { 
+    
+            return city + ', ' + state;
+          } else {
+    
+            return '';
+          }
+      }
 }
--- shrpr/src/app/template/template.component.ts
+++ shrpr/src/app/template/template.component.ts
@@ -5,7 +5,6 @@ import { Subscription } from 'rxjs/Subscription';
 import { LikeService } from "../like.service";
 import { SearchComponent } from '../search/search.component';
 
-declare var google;
 
 @Component({
   selector: 'app-template',
@@ -32,10 +31,9 @@ export class TemplateHeader implements OnInit, OnDestroy {
 
   isheaderShrunk: boolean = false;
   isBtnActive: boolean = false;
-  location: string = '';
   counter: number = 0;
   subscription: Subscription;
-  groups = ['For Fun', 'For Work', 'For Kids'];
+
 
   constructor(zone: NgZone, private likeService: LikeService) {
     window.onscroll = () => {
@@ -56,35 +54,6 @@ export class TemplateHeader implements OnInit, OnDestroy {
       this.counter = count;
     });
 
-    if (navigator.geolocation) { //check if we can get lat/lng
-
-      //create location
-      navigator.geolocation.getCurrentPosition(position => {
-
-        let lat = position.coords.latitude;
-        let lng = position.coords.longitude;
-
-        let geocoder = new google.maps.Geocoder();
-        let latlng = new google.maps.LatLng(lat, lng);
-
-        let request = {
-          latLng: latlng
-        };
-
-        //reverse geocode
-        geocoder.geocode(request, (results, status) => {
-
-          if (status == google.maps.GeocoderStatus.OK) {
-
-            if (results[0] != null) {
-
-              //get location
-              this.location = this.findLocation(results[0].address_components);
-            }
-          }
-        });
-      });
-    }
   }
 
   ngOnDestroy(){
@@ -95,52 +64,7 @@ export class TemplateHeader implements OnInit, OnDestroy {
     this.isBtnActive = !this.isBtnActive;
   }
 
-  findLocation(components){      
-
-    var city = false,
-        state = false,
-        zip = false,
-        component,
-        i, l, x, y;
-
-      for(i = 0, l = components.length; i < l; ++i){
-
-        //store component
-        component = components[i];
-
-        //check each type
-        for(x = 0, y = component.types.length; x < y; ++ x){
-
-          //depending on type, assign to var
-          switch(component.types[x]){
-
-            case 'neighborhood':
-            city = component.long_name;
-            break;
-
-            case 'administrative_area_level_1':
-            state = component.short_name;
-            break;
-
-            case 'postal_code':
-            zip = component.short_name;
-            break;
-          }
-        }
-      }
-
-      if(city && state && zip){
-
-        return city + ', ' + state + ' ' + zip;
-      }
-      else if (city && state) { 
-
-        return city + ', ' + state;
-      } else {
-
-        return '';
-      }
-  }
+  
   
 }
 
