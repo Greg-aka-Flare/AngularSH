9a4d8c45afddfb987b4cddbe03d42c9db7264c9a
gagrhari <32124815+gagrhari@users.noreply.github.com>
2017-11-06 18:33:12 +0530

    call to courses api into instructor

--- shrpr/src/app/instructor-profile/instructor-profile.component.ts
+++ shrpr/src/app/instructor-profile/instructor-profile.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser'
 import { Response } from "@angular/http";
 import { ActivatedRoute, Params } from '@angular/router';
 import {trigger, state, style, transition, animate} from '@angular/animations';
 import { TabsComponent } from "../home/tabs/tabs.component";
 import { StarRatingModule } from 'angular-star-rating';
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/map';
 
+import { Course } from "../course.interface";
+import { CourseService } from "../course.service";
 import { Instructor } from "../instructor.interface";
 import { InstructorService } from "../instructor.service";
 
@@ -20,11 +23,28 @@ import { InstructorService } from "../instructor.service";
 })
 export class InstructorProfileComponent implements OnInit {
   instructors:any[];
+  courses: any[];
   private id:number;
   subscription: Subscription;
+  private _data = new BehaviorSubject<Course[]>([]);
+
+// change data to use getter and setter
+@Input()
+set data(value) {
+    // set the latest value for _data BehaviorSubject
+    this._data.next(value);
+};
+
+get data() {
+    // get the latest value from _data BehaviorSubject
+    return this._data.getValue();
+}
+
+
+
   width = document.documentElement.clientWidth;
   
-  constructor(private instructorService: InstructorService, private route: ActivatedRoute) { 
+  constructor(private instructorService: InstructorService, private route: ActivatedRoute, private courseService: CourseService) { 
   //constructor() { 
     let sub = this.route.params.subscribe((params: Params) => {
       this.id = params['id'];
@@ -42,10 +62,19 @@ export class InstructorProfileComponent implements OnInit {
     });
 
   }
-  ngOnDestroy(){
-    this.subscription.unsubscribe();
-  }
+  
   ngOnInit() {
+    this._data
+    .subscribe(x => {
+        this.courses = this.data;
+        if(this.courses) {
+          for(var i = 0, l = this.courses.length; i < l; i++) {
+            this.courses[i].state = 'default';
+            console.log('course data: '+ this.courses[i].id);
+          }
+        }
+    });
+
      this.instructorService.getInstructor(this.id)
      .subscribe(
        (response) => {
@@ -57,6 +86,10 @@ export class InstructorProfileComponent implements OnInit {
        
      );
     
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
