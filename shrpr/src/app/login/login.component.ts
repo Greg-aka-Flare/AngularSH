a3415770e9ff022dad541bb11729630ea69a0430
Carl Dawson <carl@invisible-gorilla.com>
2017-11-08 23:08:13 -0500

    Fixed bugs

--- shrpr/src/app/login/login.component.ts
+++ shrpr/src/app/login/login.component.ts
@@ -24,7 +24,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'name': new FormControl(null, [Validators.required, Validators.email]),
+      'name': new FormControl(null, Validators.required),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, Validators.required)
     });
@@ -37,9 +37,9 @@ export class LoginComponent implements OnInit {
 
   onSignup() {
 
-    const name = this.loginForm.value.name;
-    const email = this.loginForm.value.email;
-    const password = this.loginForm.value.password;
+    const name = this.signupForm.value.name;
+    const email = this.signupForm.value.email;
+    const password = this.signupForm.value.password;
 
     this.userService.signup(name, email, password)
       .subscribe(
