bf12797b1450af1b2c63a1d04443cfa84450487a
gagrhari <32124815+gagrhari@users.noreply.github.com>
2017-11-25 03:00:13 +0530

    image slider on course page added

--- shrpr/src/app/coursecard/coursecard.component.ts
+++ shrpr/src/app/coursecard/coursecard.component.ts
@@ -10,6 +10,8 @@ import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/map';
 
+
+
 import { Course } from "../course.interface";
 import { CourseService } from "../course.service";
 
@@ -23,10 +25,19 @@ export class CoursecardComponent implements OnInit, OnDestroy {
   private id: number;
   course: any;
   semesterDetails:string;
+  primaryImg:string;
+  secondaryImg:string;
   
   //subscription: Subscription;
   private subscriptions = new Subscription();
   width = document.documentElement.clientWidth;
+
+  //The time to show the next photo
+  private NextPhotoInterval:number = 5000;
+  //Looping or not
+  private noLoopSlides:boolean = false;
+  //Photos
+  private slides:Array<any> = [];
 
   constructor(
     private courseService: CourseService, 
@@ -50,10 +61,28 @@ export class CoursecardComponent implements OnInit, OnDestroy {
     this.subscriptions.add(this.courseService.getCourse(this.id).subscribe(course => {
       this.course = course;
       this.semesterDetails = JSON.parse(this.course.semesters[0].details);
-      
+      this.primaryImg = this.course.semesters[0].primary_img;
+      this.secondaryImg = JSON.parse(this.course.semesters[0].details).secondary_img;
+      this.slides.push(
+        //{image:'../../assets/img/'+ this.primaryImg},
+        //{image:'../../assets/img/'+ this.secondaryImg}
+        {image:'../../assets/img/court.jpg'},
+        {image:'../../assets/img/court-two.jpg'}
+      );
+
     }));
-  }
+
   
+
+  }
+ 
+  private removeLastSlide() {
+    this.slides.pop();
+  } 
+   
+
+  
+
   ngOnDestroy(){
     this.subscriptions.unsubscribe();
   }
