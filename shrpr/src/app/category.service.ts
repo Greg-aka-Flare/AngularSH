5f8a3d5fd8cd8da8360615862c5f911816b0efec
Carl Dawson <carl@invisible-gorilla.com>
2017-11-30 05:05:50 -0500

    Added categories to search functionality

---
+++ shrpr/src/app/category.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/Rx';
+import { Observable } from "rxjs";
+
+@Injectable()
+export class CategoryService {
+
+	url: string = 'http://shrpr.jdapwnzhx7.us-east-2.elasticbeanstalk.com/api/';
+
+	constructor(private http: HttpClient) {}
+
+	getCategories(): Observable<any> {
+
+		//create api endpoint
+		let api = this.url + 'categories';
+
+		//get categories
+		return this.http.get(api)
+    .map(function(data: any) {
+
+      return data = data.categories;
+    });
+	}
+}
