8b52d2ccc0d531278ee346407daa51095519ad84
Carl Dawson <carl@invisible-gorilla.com>
2017-12-26 03:09:06 -0500

    Added filter for desktop sidebar, mobile edits

--- shrpr/src/app/core/curious/curious.component.ts
+++ shrpr/src/app/core/curious/curious.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser'
-import { Response } from "@angular/http";
-
 import { Observable } from 'rxjs/Observable';
 
 @Component({
@@ -10,21 +7,20 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./curious.component.css']
 })
 export class CuriousComponent implements OnInit {
+
   width = document.documentElement.clientWidth;
-  constructor() {
+
+  constructor() {}
+
+  ngOnInit() {
     
     const $resizeEvent = Observable.fromEvent(window, 'resize')
     .map(() => {
       return document.documentElement.clientWidth;
-      })
+    });
     
     $resizeEvent.subscribe(data => {
       this.width = data;
     });
-    
-  }
-
-  ngOnInit() {
-
   }
 }
--- shrpr/src/app/shared/filter/filter.component.ts
+++ shrpr/src/app/shared/filter/filter.component.ts
@@ -1,23 +1,50 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit, Input, Output, EventEmitter, HostListener, ElementRef } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.css']
+  styleUrls: ['./filter.component.css'],
+  animations: [
+    trigger('scrollAnimation', [
+      state('show', style({
+        opacity: 1
+      })),
+      state('hide',   style({
+        opacity: 0
+      })),
+      transition('show => hide', animate('200ms ease-out')),
+      transition('hide => show', animate('200ms ease-in'))
+    ])
+  ]
 })
 export class FilterComponent implements OnInit {
 
 	@Input('courses') courses: any[];
 	@Output() onFilter: EventEmitter<any> = new EventEmitter();
 	filterForm: FormGroup;
+  showFilter: boolean = false;
 	filtered: any[] = [];
 	courseCount: number = 0;
 	instructors: any[] = [];
+  state: string = 'hide';
 
-  constructor() {}
+  constructor(public formBuilder: FormBuilder, public el: ElementRef) {}
 
   ngOnInit() {
+
+    //get count
+    this.courseCount = this.courses.length;
+
+    //create form
+    this.filterForm = this.formBuilder.group({
+      'fun': [true],
+      'work': [true],
+      'kids': [true]
+    });
+
+    let i = 0;
 
   	//for each course, sort data
   	for(let course of this.courses){
@@ -26,25 +53,33 @@ export class FilterComponent implements OnInit {
   		if(!this.containsObject(course.instructor, this.instructors)){
 
 	  		this.instructors.push(course.instructor);
+
+        this.filterForm.addControl('instructor_' + i, new FormControl(true));
+
+        ++i;
   		}
   	}
-
-  	console.log(this.instructors);
-
-  	//get count
-  	this.courseCount = this.courses.length;
-
-  	//create form
-    this.filterForm = new FormGroup({
-      'fun': new FormControl(true),
-      'work': new FormControl(true),
-      'kids': new FormControl(true)
-    });
  
     //subscribe to changes
     this.filterForm.valueChanges.subscribe(  
       (form: any) => this.filter(form)
     );
+  }
+
+  @HostListener('window:scroll', ['$event']) checkScroll() {
+
+    const componentPosition = this.el.nativeElement.parentElement.offsetTop;
+    const scrollPosition = window.pageYOffset;
+
+    if(scrollPosition >= componentPosition) {
+      this.state = 'show'
+    } else {
+      this.state = 'hide'
+    }
+  }
+
+  toggleFilter() {
+    this.showFilter = (this.showFilter) ? false : true;
   }
 
   filter(form: any) {
