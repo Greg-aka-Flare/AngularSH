e2bea77fbb9d36bc917af5898becc9653bc92e06
gagrhari <32124815+gagrhari@users.noreply.github.com>
2017-12-27 20:15:50 +0530

    login redirect link update

--- shrpr/src/app/core/login/login.component.ts
+++ shrpr/src/app/core/login/login.component.ts
@@ -102,7 +102,7 @@ export class LoginComponent implements OnInit, OnDestroy {
     const profile = this.ProfileForm.value.signuptype;
     //console.log(profile);
     if(profile == 'student'){
-      this.router.navigateByUrl('course-select');
+      this.router.navigateByUrl('profile/course-select');
     }
     if(profile == 'instructor'){
       this.router.navigateByUrl('instructor/2');
