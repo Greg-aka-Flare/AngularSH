d6d13060451d0df768f23aefddda4be198fcba1c
gagrhari <32124815+gagrhari@users.noreply.github.com>
2017-12-29 21:18:30 +0530

    course date generated as per the start date and session no.

--- shrpr/src/app/profile/instructor-course/instructor-course.component.ts
+++ shrpr/src/app/profile/instructor-course/instructor-course.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef, NgModule, Renderer } from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-instructor-course',
@@ -7,7 +10,13 @@ import {BrowserModule} from '@angular/platform-browser';
   styleUrls: ['./instructor-course.component.css']
 })
 export class InstructorCourseComponent implements OnInit {
-  //@ViewChild('panel') panel : ElementRef;
+
+  
+  
+  instructorCourseForm: FormGroup;
+  private sessionArray = new Array<{sessionDate:string, startTime: string, endTime: string}>();
+  //sessionArray: Array<{sessionDate:string, startTime: string, endTime: string}>;
+  //private sessionArray = new Array<{sessionDate:string}>();
 
   @ViewChild('panel') panel : ElementRef
   
@@ -15,15 +24,36 @@ export class InstructorCourseComponent implements OnInit {
   lastSlideNo:number = 4;
   prevPos: string = '';
   nextPos:number = 0;
+  
+  courseStartTimeText:string;
+  courseSessionNumber:number;
+  courseDurationNumber:number;
 
-  constructor(public renderer: Renderer) { }
+
+  constructor(public renderer: Renderer) {
+    
+   }
 
   ngOnInit() {
-    
+    this.instructorCourseForm = new FormGroup({
+      'courseGroupSelect': new FormControl(null, Validators.required),
+      'courseCategorySelect': new FormControl(null, [Validators.required]),
+      'courseSubCategorySelect': new FormControl(null, Validators.required),
+      'courseTitleText': new FormControl(null, Validators.required),
+      'courseDescriptionText': new FormControl(null, Validators.required),
+      'courseStartDateText': new FormControl(null, Validators.required),
+      'courseStartTimeText': new FormControl(null, Validators.required),
+      'courseSessionNumber': new FormControl(null, Validators.required),
+      'courseDurationNumber': new FormControl(null, Validators.required),
+      'courseLocationText': new FormControl(null, Validators.required),
+      'courseEndTimeText': new FormControl(null)
+    });
   }
 
   nextSlide(){
-    
+    if(this.slideNo == 3){
+      this.sessionDetailsinit();
+    }
     if( this.slideNo > 0 && this.slideNo < this.lastSlideNo ){
       this.slideNo++;
       this.renderer.setElementStyle(
@@ -47,6 +77,35 @@ export class InstructorCourseComponent implements OnInit {
           'translateX(0px)');
       }
     }
+  
+  sessionDetailsinit(){
+    let courseStartDateText = this.instructorCourseForm.value.courseStartDateText;
+    let courseStartTimeText = this.instructorCourseForm.value.courseStartTimeText;
+    let courseEndTimeText = this.instructorCourseForm.value.courseEndTimeText;
+    this.courseSessionNumber = this.instructorCourseForm.value.courseSessionNumber;
+    let courseDurationNumber = this.instructorCourseForm.value.courseDurationNumber;
+    
+    courseStartTimeText = moment(courseStartTimeText+':00', 'hh:mm:ss a');
+    courseStartTimeText = moment(courseStartTimeText).format('LT');
+
+    courseEndTimeText = moment(courseStartTimeText, 'LT').add(courseDurationNumber, 'hours');
+    
+    courseEndTimeText = moment(courseEndTimeText).format('LT');
+    
+    courseStartDateText = moment(courseStartDateText).format('DD-MM-YYYY');
+
+    this.sessionArray.push(courseStartDateText, courseStartTimeText, courseEndTimeText);
+    for(var i = 0; i < this.courseSessionNumber; i++){
+    
+      courseStartDateText = moment(courseStartDateText, 'DD-MM-YYYY').add(7, 'days').calendar();
+      courseStartDateText = moment(courseStartDateText).format('DD-MM-YYYY');
+      this.sessionArray.push(courseStartDateText, courseStartTimeText, courseEndTimeText);
+      
+    }
+    console.log(this.sessionArray);
+  }  
+   
+    
   }
 
 
