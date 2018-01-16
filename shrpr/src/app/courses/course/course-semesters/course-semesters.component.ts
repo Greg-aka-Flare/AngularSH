2d793b8c321cf8e47ace11228e74eff8cf9567a4
gagrhari <32124815+gagrhari@users.noreply.github.com>
2018-01-17 02:28:48 +0530

    All Semester redirected to new page

--- shrpr/src/app/courses/course/course-semesters/course-semesters.component.ts
+++ shrpr/src/app/courses/course/course-semesters/course-semesters.component.ts
@@ -1,16 +1,45 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import { Subscription } from 'rxjs/Subscription';
+
+
 import { CourseService } from '../../../courses/course.service';
+import { CourseComponent } from '../../course/course.component';
+
 
 @Component({
   selector: 'app-course-semesters',
   templateUrl: './course-semesters.component.html',
   styleUrls: ['./course-semesters.component.css']
 })
-export class CourseSemestersComponent implements OnInit {
+export class CourseSemestersComponent implements OnInit, OnDestroy {
+  private id: number;
   @Input('course') course: any;
-  constructor(private courseService: CourseService) { }
+  private subscriptions = new Subscription();
+  constructor(
+    //private courseComponent: CourseComponent,
+    private courseService: CourseService,
+    private route: ActivatedRoute
+  ) { 
+    
+  }
 
   ngOnInit() {
+    this.subscriptions.add(this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+      this.courseService.getCourse(this.id).subscribe(course => {
+        this.course = course;
+      })
+
+    }));
+    //this.course = this.courseComponent.course;
+    
+  }
+
+  ngOnDestroy(){
+    this.subscriptions.unsubscribe();
   }
 
 }
--- shrpr/src/app/courses/courses-routing.module.ts
+++ shrpr/src/app/courses/courses-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
+import { CourseSemestersComponent } from './course/course-semesters/course-semesters.component';
 import { CourseListComponent } from './course-list/course-list.component';
 
 
 const coursesRoutes: Routes = [
 	{ path: '', component: CoursesComponent, children: [
+		{ path: ':id/semesters', component: CourseSemestersComponent },
 		{ path: 'list/:group', component: CourseListComponent },
 		{ path: ':id', component: CourseComponent }
 	] }
