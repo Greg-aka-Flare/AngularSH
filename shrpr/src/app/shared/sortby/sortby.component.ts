9bacd91967ac8f3235c41b839a94f2c55eef40aa
oshankpandey <32125530+oshankpandey@users.noreply.github.com>
2018-02-10 03:10:54 +0530

    filter box sticky fix

--- shrpr/src/app/shared/sortby/sortby.component.ts
+++ shrpr/src/app/shared/sortby/sortby.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener} from "@angular/core";
 
 @Component({
   selector: 'app-sortby',
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sortby.component.css']
 })
 export class SortbyComponent implements OnInit {
+  isfilterShrunk: boolean = false;
 
   constructor() { }
-
+  
+  @HostListener('window:scroll')onscroll = () => {
+    if(window.pageYOffset > 0) {
+         this.isfilterShrunk = true;
+    } else {
+         this.isfilterShrunk = false;
+    }
+  }
   ngOnInit() {
   }
 
