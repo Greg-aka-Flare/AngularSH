dbf9f93ba6d27f6764964d51de04937290c2ffd2
gagrhari <32124815+gagrhari@users.noreply.github.com>
2018-02-12 19:05:55 +0530

    commented code removed from home component

--- shrpr/src/app/core/home/home.component.ts
+++ shrpr/src/app/core/home/home.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { NgModel } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { trigger, state, style, transition, animate } from '@angular/animations';
-import { Subscription } from 'rxjs/Subscription';
-import { Observable } from 'rxjs/Observable';
 
-import { Course, CourseService } from '@app/core';
 
 @Component({
   selector: 'app-home',
@@ -15,26 +9,10 @@ import { Course, CourseService } from '@app/core';
 })
 
 export class HomeComponent implements OnInit {
-  private subscriptions = new Subscription();
-  width = document.documentElement.clientWidth;
-
-  isheaderShrunk: boolean = false;
-  groups = ['For Fun', 'For Work', 'For Kids'];
   
   constructor() {
-      const $resizeEvent = Observable.fromEvent(window, 'resize')
-      .map(() => {
-        return document.documentElement.clientWidth;
-        })
-      
-      this.subscriptions.add($resizeEvent.subscribe(data => {
-        this.width = data;
-      }));
   }
 
   ngOnInit() { }
 
-  ngOnDestroy(){
-    this.subscriptions.unsubscribe();
-  }
 }
